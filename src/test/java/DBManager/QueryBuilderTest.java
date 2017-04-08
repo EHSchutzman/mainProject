e50ecc5444799dbc02c658fa0816d2c82720e237
Daniel Kim <daniel.jaewoo.kim@gmail.com>
2017-04-08 17:36:35 -0400

    Updated DBManager

--- src/test/java/DBManager/QueryBuilderTest.java
+++ src/test/java/DBManager/QueryBuilderTest.java
@@ -1,5 +1,7 @@
 package DBManager;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -8,6 +10,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class QueryBuilderTest {
     @org.junit.jupiter.api.Test
     void createInsertStatement() {
+        QueryBuilder qb = new QueryBuilder();
+        ArrayList<String> fields = new ArrayList<>();
+        fields.add("col1=val1");
+        fields.add("col2=val2");
+        String qStr = qb.createInsertStatement("USER", fields);
+        assertEquals(qStr, "insert into USER values(col1=val1,col2=val2);");
     }
 
     @org.junit.jupiter.api.Test
