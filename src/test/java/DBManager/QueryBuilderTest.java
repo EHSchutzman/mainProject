4e08278c133121b9f808648a19fdb38aec8a1ff8
Leo Bunyea <lrbunyea@wpi.edu>
2017-04-08 15:43:20 -0400

    Created new package for database subsystem with testing files, added Dan's code for query builder

---
+++ src/test/java/DBManager/QueryBuilderTest.java
@@ -0,0 +1,21 @@
+package DBManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Leo on 4/8/2017.
+ */
+class QueryBuilderTest {
+    @org.junit.jupiter.api.Test
+    void createInsertStatement() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void createUpdateStatement() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void createSelectStatement() {
+    }
+
+}
