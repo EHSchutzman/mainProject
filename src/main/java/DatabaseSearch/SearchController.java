40ebb35215ab9841e6e5992dc7b30b99c11fa4ff
Daniel Kim <daniel.jaewoo.kim@gmail.com>
2017-04-18 19:18:24 -0400

    Fixed ChoiceBox dynamic update

--- src/main/java/DatabaseSearch/SearchController.java
+++ src/main/java/DatabaseSearch/SearchController.java
@@ -3,8 +3,11 @@ package DatabaseSearch;
 import DBManager.DBManager;
 import Initialization.Main;
 import UserAccounts.User;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
@@ -345,6 +348,12 @@ public class SearchController {
     public void initUserAuthenticationChoiceBox() {
         authentication_filter.getItems().addAll("0", "1", "2", "3");
         authentication_filter.setValue("0");
+        authentication_filter.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                searchUsers();
+            }
+        });
     }
 
     @FXML
