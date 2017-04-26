61ec36c37958246e08c87399418652b77a24074a
ethanmarshall <etmarshall@wpi.edu>
2017-04-26 17:58:22 -0400

    implemented colon delimited csv/text file generator

--- src/main/java/Controllers/csvOptionsController.java
+++ src/main/java/Controllers/csvOptionsController.java
@@ -54,6 +54,77 @@ public class csvOptionsController extends UIController{
         }
     }
 
+    @FXML
+    public void makeColon1() {
+        manager.generateCSV(observableList, ":", ".txt");
+        try {
+            displayConfirmationMessage();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void makeColon() {
+        ObservableList<AppRecord> observer = observableList;
+        String appID;
+        String brandName;
+        String completeDate;
+        String fancifulName;
+        String formID;
+        String permitNo;
+        String serialNo;
+        String status;
+        String ttbID;
+        String typeID;
+
+        for(AppRecord app : observer) {
+            appID = app.getApplicantID();
+            //ApplicantID
+            appID = "\"" + appID + "\"";
+            app.setApplicantID(appID);
+            //BrandName
+            brandName = app.getBrandName();
+            brandName = "\"" + brandName + "\"";
+            app.setBrandName(brandName);
+            //CompletedDate
+            completeDate = app.getCompletedDate();
+            completeDate = "\"" + completeDate + "\"";
+            app.setCompletedDate(completeDate);
+            //FancifulName
+            fancifulName = app.getFancifulName();
+            fancifulName = "\"" + fancifulName + "\"";
+            app.setFancifulName(fancifulName);
+            //FormID
+            formID = app.getFormID();
+            formID = "\"" + formID + "\"";
+            app.setFormID(formID);
+            //PermitNo
+            permitNo = app.getPermitNo();
+            permitNo = "\"" + permitNo + "\"";
+            app.setPermitNo(permitNo);
+            //SerialNo
+            serialNo = app.getSerialNo();
+            serialNo = "\"" + serialNo + "\"";
+            app.setSerialNo(serialNo);
+            //GetStatus
+            status = app.getStatus();
+            status = "\"" + status + "\"";
+            app.setStatus(status);
+            //TtbID
+            ttbID = app.getTtbID();
+            ttbID = "\"" + ttbID + "\"";
+            app.setTtbID(ttbID);
+            //TypeID
+            typeID = app.getTypeID();
+            typeID = "\"" + typeID + "\"";
+            app.setTypeID(typeID);
+
+        }
+        System.out.println(observableList.get(0).getBrandName());
+        manager.generateCSV(observableList, ":", ".txt");
+
+    }
     /**
      * Function makes a tab delimited format text file out of observable list in this controller.
      */
