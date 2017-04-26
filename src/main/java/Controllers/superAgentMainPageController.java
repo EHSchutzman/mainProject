45dae9413d33c05be3e4a310a104371821013c49
Daniel Kim <daniel.jaewoo.kim@gmail.com>
2017-04-26 11:09:22 -0400

    finished super agent controllers

--- src/main/java/Controllers/superAgentMainPageController.java
+++ src/main/java/Controllers/superAgentMainPageController.java
@@ -1,11 +1,17 @@
 package Controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * Status: empty, needs work.
  * TODO: finish up functionality for this - need to redo superAgent functionality + UI pages
+ * TODO: is this what we really want?
  */
 public class superAgentMainPageController extends UIController{
 
@@ -13,11 +19,44 @@ public class superAgentMainPageController extends UIController{
     private Button reviewLabelButton, userSearchButton, createAgentButton;
 
     @FXML
-    public void displayReviewApplication() {} // Stub TODO: remove and replace with actual
+    public void setDisplayApplicationReview() throws IOException {
+        Stage stage;
+        stage=(Stage) reviewLabelButton.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("applicationStatusForApplicant.fxml"));
+        System.out.println(loader.getLocation().getPath());
+        Scene scene = new Scene(loader.load());
+        stage.setScene(scene);
+        stage.show();
+        applicationStatusForApplicantController controller = loader.getController();
+        controller.init(super.main);
+    }
 
     @FXML
-    public void displayUserSearchPage() {} // Stub TODO: remove and replace with actual
+    public void setDisplayToSuperAgentSearchUsers() throws IOException {
+        Stage stage;
+        stage=(Stage) userSearchButton.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("superAgentSearchUsers.fxml"));
+        System.out.println(loader.getLocation().getPath());
+        Scene scene = new Scene(loader.load());
+        stage.setScene(scene);
+        stage.show();
+        superAgentSearchUsersController controller = loader.getController();
+        controller.init(super.main);
+        controller.initUserAuthenticationChoiceBox();
+        controller.displayResults();
+    }
 
+    // TODO: change to createUser page? is this what we really want?
     @FXML
-    public void displayCreateAgentPage() {} // Stub TODO: remove and replace with actual
+    public void setDisplayToSuperAgentCreateAgent() throws IOException {
+        Stage stage;
+        stage=(Stage) createAgentButton.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("superAgentCreateAgent.fxml"));
+        System.out.println(loader.getLocation().getPath());
+        Scene scene = new Scene(loader.load());
+        stage.setScene(scene);
+        stage.show();
+        superAgentCreateAgentController controller = loader.getController();
+        controller.init(super.main);
+    }
 }
