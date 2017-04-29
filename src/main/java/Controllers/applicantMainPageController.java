32f260c363c67312d69ff80120c59d072ea4d825
Erick Moo <zmoofeng@wpi.edu>
2017-04-29 17:23:00 -0400

    Made put slideshow functions in a separate class
    animations stop when pressing on a button that changes pages

--- src/main/java/Controllers/applicantMainPageController.java
+++ src/main/java/Controllers/applicantMainPageController.java
@@ -37,11 +37,13 @@ public class applicantMainPageController extends UIController{
     int count = 0;
 
     @FXML
-    private HBox imagebox;
+    public HBox imagebox;
     @FXML
     private Button viewFormsButton;
     @FXML
     private Button submissionButton;
+
+    slideshowController slideshow = new slideshowController();
 
     @FXML
     public void setDisplayToSearchPage() throws IOException {
@@ -73,6 +75,7 @@ public class applicantMainPageController extends UIController{
         pane.setTop(menuBarSingleton.getInstance().getBar());
         pane.setCenter(loader.load());
         stage.setScene(scene);
+        slideshow.stopAnimation();
         stage.show();
         applicationStatusForApplicantController controller = loader.getController();
         controller.init(super.main);
@@ -100,6 +103,7 @@ public class applicantMainPageController extends UIController{
 
         root.setBottom(pane);
         stage.setScene(root.getScene());
+        slideshow.stopAnimation();
         stage.show();
 
 
@@ -123,102 +127,5 @@ public class applicantMainPageController extends UIController{
         stage.setScene(scene);
         stage.show();
     }*/
-
-    public void initSlideshow() {
-        // load the images
-        imageArrayList.add(new ImageView("file:images/AZOI.jpg"));
-        System.out.println(imageArrayList.get(0).getImage().isError());
-        imageArrayList.get(0).setFitWidth(500);
-        imageArrayList.get(0).setFitHeight(500);
-        imageArrayList.add(new ImageView("file:images/BIG BLONDE.jpg"));
-        System.out.println(imageArrayList.get(1).getImage().isError());
-        imageArrayList.get(1).setFitWidth(500);
-        imageArrayList.get(1).setFitHeight(500);
-        imageArrayList.add(new ImageView("file:images/CHEVALLON.jpg"));
-        System.out.println(imageArrayList.get(2).getImage().isError());
-        imageArrayList.get(2).setFitWidth(500);
-        imageArrayList.get(2).setFitHeight(500);
-        imageArrayList.add(new ImageView("file:images/founders.jpg"));
-        System.out.println(imageArrayList.get(3).getImage().isError());
-        imageArrayList.get(3).setFitWidth(500);
-        imageArrayList.get(3).setFitHeight(500);
-        imageArrayList.add(new ImageView("file:images/FALORNI.jpg"));
-        System.out.println(imageArrayList.get(4).getImage().isError());
-        imageArrayList.get(4).setFitWidth(500);
-        imageArrayList.get(4).setFitHeight(500);
-
-        imagebox.getChildren().addAll(imageArrayList);
-
-        imagebox.setOnMouseClicked(event -> {
-            if (event.getClickCount() == 1 && imageArrayList.get(count).getImage().impl_getUrl().equals("file:src/sample/image/bacardi.jpg")) {
-                final Stage dialog = new Stage();
-                VBox dialogVbox = new VBox(20);
-                dialogVbox.getChildren().add(new Text("This is a Dialog"));
-                Scene dialogScene = new Scene(dialogVbox, 300, 200);
-                dialog.setScene(dialogScene);
-                dialog.show();
-            }
-        });
-    }
-
-    public void startAnimation() {
-        //error occurred on (ActionEvent t) line
-        //slide action
-        EventHandler<ActionEvent> slideAction = (ActionEvent t) -> {
-            count++;
-            System.out.println("slide");
-            TranslateTransition trans = new TranslateTransition(Duration.seconds(1.5), imagebox);
-            trans.setByX(-500);
-            trans.setInterpolator(Interpolator.EASE_BOTH);
-            trans.play();
-        };
-        //eventHandler
-        EventHandler<ActionEvent> resetAction = (ActionEvent t) -> {
-            count = 0;
-            TranslateTransition trans = new TranslateTransition(Duration.seconds(1), imagebox);
-            trans.setByX((imageArrayList.size() - 1) * 500);
-            trans.setInterpolator(Interpolator.EASE_BOTH);
-            trans.play();
-        };
-
-        List<KeyFrame> keyFrames = new ArrayList<>();
-        for (int i = 1; i <= imageArrayList.size(); i++) {
-            if (i == imageArrayList.size()) {
-                keyFrames.add(new KeyFrame(Duration.seconds(i * 5), resetAction));
-            } else {
-                keyFrames.add(new KeyFrame(Duration.seconds(i * 5), slideAction));
-            }
-        }
-
-        //add timeLine
-        Timeline anim = new Timeline(keyFrames.toArray(new KeyFrame[imageArrayList.size()]));
-
-        viewFormsButton.setOnMouseClicked(event -> {
-            System.out.println("stopping anim");
-            if (event.getClickCount() == 1) {
-                anim.stop();
-                try {
-                    setDisplayToApplicationStatusForApplicant();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        submissionButton.setOnMouseClicked(event -> {
-            System.out.println("stopping anim");
-            if (event.getClickCount() == 1) {
-                anim.stop();
-                try {
-                    setDisplayToIter2application();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        anim.setCycleCount(Timeline.INDEFINITE);
-        anim.playFromStart();
-    }
 
 }
--- src/main/java/Controllers/mainPageController.java
+++ src/main/java/Controllers/mainPageController.java
@@ -32,15 +32,15 @@ import java.util.List;
  * TODO: change about link to button? - have a function setDisplayToAboutPage in UIController
  */
 public class mainPageController extends UIController {
-    ArrayList<ImageView> imageArrayList = new ArrayList<>();
-    int count = 0;
 
     @FXML
     private Button loginButton; // This is a different loginButton from UIController's? double check
     @FXML
-    private HBox imagebox;
+    public HBox imagebox;
     @FXML
     private Hyperlink aboutLink;
+
+    public slideshowController slideshow = new slideshowController();
 
     @FXML
     private void setDisplayToSearch() throws IOException{
@@ -82,6 +82,7 @@ public class mainPageController extends UIController {
         stage.setScene(scene);
         borderPane.setTop(menuBarSingleton.getInstance().getBar());
         borderPane.setBottom(anchorPane);
+        slideshow.stopAnimation();
         stage.show();
         loginPageController controller = loader.getController();
         controller.init(super.main);
@@ -101,90 +102,5 @@ public class mainPageController extends UIController {
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
-    }
-
-    public void initSlideshow() {
-        // load the images
-        imageArrayList.add(new ImageView("file:images/AZOI.jpg"));
-        System.out.println(imageArrayList.get(0).getImage().isError());
-        imageArrayList.get(0).setFitWidth(500);
-        imageArrayList.get(0).setFitHeight(500);
-        imageArrayList.add(new ImageView("file:images/BIG BLONDE.jpg"));
-        System.out.println(imageArrayList.get(1).getImage().isError());
-        imageArrayList.get(1).setFitWidth(500);
-        imageArrayList.get(1).setFitHeight(500);
-        imageArrayList.add(new ImageView("file:images/CHEVALLON.jpg"));
-        System.out.println(imageArrayList.get(2).getImage().isError());
-        imageArrayList.get(2).setFitWidth(500);
-        imageArrayList.get(2).setFitHeight(500);
-        imageArrayList.add(new ImageView("file:images/founders.jpg"));
-        System.out.println(imageArrayList.get(3).getImage().isError());
-        imageArrayList.get(3).setFitWidth(500);
-        imageArrayList.get(3).setFitHeight(500);
-        imageArrayList.add(new ImageView("file:images/FALORNI.jpg"));
-        System.out.println(imageArrayList.get(4).getImage().isError());
-        imageArrayList.get(4).setFitWidth(500);
-        imageArrayList.get(4).setFitHeight(500);
-
-        imagebox.getChildren().addAll(imageArrayList);
-
-        imagebox.setOnMouseClicked(event -> {
-            if (event.getClickCount() == 1 && imageArrayList.get(count).getImage().impl_getUrl().equals("file:src/sample/image/bacardi.jpg")) {
-                final Stage dialog = new Stage();
-                VBox dialogVbox = new VBox(20);
-                dialogVbox.getChildren().add(new Text("This is a Dialog"));
-                Scene dialogScene = new Scene(dialogVbox, 300, 200);
-                dialog.setScene(dialogScene);
-                dialog.show();
-            }
-        });
-    }
-
-    public void startAnimation() {
-        //error occurred on (ActionEvent t) line
-        //slide action
-        EventHandler<ActionEvent> slideAction = (ActionEvent t) -> {
-            count++;
-            System.out.println("slide");
-            TranslateTransition trans = new TranslateTransition(Duration.seconds(1.5), imagebox);
-            trans.setByX(-500);
-            trans.setInterpolator(Interpolator.EASE_BOTH);
-            trans.play();
-        };
-        //eventHandler
-        EventHandler<ActionEvent> resetAction = (ActionEvent t) -> {
-            count = 0;
-            TranslateTransition trans = new TranslateTransition(Duration.seconds(1), imagebox);
-            trans.setByX((imageArrayList.size() - 1) * 500);
-            trans.setInterpolator(Interpolator.EASE_BOTH);
-            trans.play();
-        };
-
-        List<KeyFrame> keyFrames = new ArrayList<>();
-        for (int i = 1; i <= imageArrayList.size(); i++) {
-            if (i == imageArrayList.size()) {
-                keyFrames.add(new KeyFrame(Duration.seconds(i * 5), resetAction));
-            } else {
-                keyFrames.add(new KeyFrame(Duration.seconds(i * 5), slideAction));
-            }
-        }
-
-        //add timeLine
-        Timeline anim = new Timeline(keyFrames.toArray(new KeyFrame[imageArrayList.size()]));
-
-        loginButton.setOnMouseClicked(event -> {
-            System.out.println("stopping anim");
-            if (event.getClickCount() == 1) {
-                anim.stop();
-                try {
-                    setDisplayToLoginPage();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        anim.setCycleCount(Timeline.INDEFINITE);
-        anim.playFromStart();
-
     }
 }
